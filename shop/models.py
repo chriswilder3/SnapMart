767fb98d599ad7dd4baa2e6eafe0939373b034e1
chriswilder3 <chriswilderdelta19@gmail.com>
2024-11-30 16:44:12 +0530

    created Product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField( max_length = 255)
+    description = models.TextField()
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField( max_length = 200)
+
+    image = models.CharField( max_length = 300)
+    # Why? Because most websites store images on different servers
+    # and store only urls on main site
